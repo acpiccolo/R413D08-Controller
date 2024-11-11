a718bc8cc7bff28c3aa66ffb47f1ddb16f205cf3
acpiccolo <60399193+acpiccolo@users.noreply.github.com>
2024-11-11 21:06:32 +0100

    chore: change positional arguments to named arguments

--- src/error.rs
+++ src/error.rs
@@ -3,15 +3,15 @@ use crate::protocol;
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error(
-        "The port number {0} is outside the permissible range of {} to {}",
-        protocol::PORT_NUMBER_MIN,
-        protocol::PORT_NUMBER_MAX
+        "The port number {0} is outside the permissible range of {min} to {max}",
+        min = protocol::PORT_NUMBER_MIN,
+        max = protocol::PORT_NUMBER_MAX
     )]
     PortOutOfRange(u8),
     #[error(
-        "The address value {0} is outside the permissible range of {} to {}",
-        protocol::ADDRESS_MIN,
-        protocol::ADDRESS_MAX
+        "The address value {0} is outside the permissible range of {min} to {max}",
+        min = protocol::ADDRESS_MIN,
+        max = protocol::ADDRESS_MAX
     )]
     AddressOutOfRange(u8),
 }
